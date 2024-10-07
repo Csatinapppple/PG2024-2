ce5f445ad448bc18b3935efc93ad0c02780b050b
cong <srilanks@as.s>
2024-10-07 19:20:02 -0300

    successful file read

--- jogo/src/main.cpp
+++ jogo/src/main.cpp
@@ -4,19 +4,22 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+#include "../include/file_tools.hpp"
+
 GLfloat vertices[] = {
 	-0.5, 0.5, 0,
 	0.5, -0.5, 0,
 	0.5, 0.5, 0
-}
+};
 
 GLuint vertexShader;
-const GLchar *vertexShaderSource = readFile("./shaders/vertexShader.cpp");
+std::string vertexShaderSource = readFile("./src/shaders/vertexShader.glsl");
 
 int main() {
+
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
-
+	std::cout << vertexShaderSource;
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
