38080cf298c73e9a0e26540db65e6588c32c0b5f
cuds <ana@r.c>
2024-12-01 22:04:38 +0100

    added eyeglasses and functions.py

--- trabalhogb/main.py
+++ trabalhogb/main.py
@@ -1,18 +1,23 @@
 import numpy as np
 import cv2 as cv
+import functions
 
 def main():
     img = cv.imread('baboon.png')
-    if img is None:
-        print("Image not found or unable to open!")
-        return
     
+    foreground = cv.imread('eyeglasses.png', cv.IMREAD_UNCHANGED)
+    original_img = img.copy()
+    stickers_positions = []
+
     cv.imshow("cong", img)
+    cv.setMouseCallback('cong', functions.mouse_click)
+    
 
     while True:
         # Wait for a key press or window close
         key = cv.waitKey(1)
         
+
         # Exit the loop if the window is closed or the user presses 'q'
         if cv.getWindowProperty("cong", cv.WND_PROP_VISIBLE) < 1:
             break
